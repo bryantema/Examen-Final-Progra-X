786d42ecffd9dd80368f2334f13aaa2bdbc0262c
bryantema <52361040+bryantema@users.noreply.github.com>
2022-07-02 11:13:42 -0600

    Add files via upload

---
+++ Bryan Examen Final/Models/Models/Rubromodels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Models
+{
+   public class Rubromodels
+    {
+        public int idrubro { get; set; }
+        public int descripcion { get; set; }
+        public int nombre { get; set; }
+        public int cantidad { get; set; }
+    }
+}
---
+++ Bryan Examen Final/Servises/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Servises
+{
+    public class Class1
+    {
+    }
+}
